57848ff7769c1465fc366df51247d47e43f1019f
Mackinlay Kevin <kemack83@gmail.com>
2024-09-01 22:32:06 -0300

    creating a todo list. Working on todo component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Todo from "./components/Todo"
+
+
+const App = () => {
+  return (
+    <div className="bg-gray-800 grid py-4 min-h-screen">
+      <Todo />
+    </div>
+  );
+}
+
+export default App
